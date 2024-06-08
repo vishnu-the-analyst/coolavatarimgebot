5001bcb99e9d781c49183dcebb1b6e88a63bc3ea
vishnu-the-analyst <vishnu.k.eswaran@gmail.com>
2024-06-08 21:26:14 +0200

    first commit

---
+++ telebot/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  8 19:48:04 2024
+
+@author: Vishnu Kumar
+"""
+
---
+++ telebot/credentials.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  8 19:47:52 2024
+
+@author: Vishnu Kumar
+"""
+
+bot_token = "7467905983:AAHWFT-EsSEZvQoplLK4iYswr81GHTEgYbU"
+bot_user_name = "coolavatarimgebot"
+URL = "https://coolavatarimgebot-2052f99a9ec2.herokuapp.com/"
